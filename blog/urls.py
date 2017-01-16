61033136a5a7c6cf3572a95be8f5a5d2ec8a6fb4
CapitanLiteral <anaraguen@gmail.com>
2017-01-16 15:25:14 +0100

    Something working...

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # ex: /blog/5/
+    url(r'^(?P<post_id>[0-9]+)/$', views.post_page, name='post_page'),
+    url(r'^author', views.author, name='author'),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
-from django.shortcuts import render
+
 
 # Create your views here.
+def index(request):
+    post_list = Post.objects.order_by('created_date')[:5]
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def post_page(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/post.html', {'post': post})
+
+def author(request):
+    return render(request, 'blog/about.html')
